eb352cc58e0dbb7123a0e9d055beee9dbe60f3b8
Jamie Branch <schwung988@yahoo.com>
2023-11-02 00:30:25 -0400

    fixed category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
       }
     ]
   })
-  .then(dbCategoryData => res.json(dbCatgoryData))
+  .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
